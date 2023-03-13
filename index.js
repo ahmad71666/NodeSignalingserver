e8a2cb39ffb0374168070031e6404ebee70af621
ahmedRafique-at-636656648246 <ahmedRafique-at-636656648246>
2023-03-13 23:55:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { Server } = require("socket.io")
+
+const io = new Server({
+    cors: true
+})
+const app = express()
+
+app.use(bodyParser.json())
+
+const userIdToSocketMapping = new Map()
+const socketMappingToUserId = new Map()
+
+io.on("connection", (socket) => {
+
+    console.log("New Connection Socket");
+
+    socket.on("join-room", (data) => {
+
+        userIdToSocketMapping.set(data.userID, socket.id)
+        socketMappingToUserId.set(socket.id, data.userID)
+        socket.join("room1")
+    })
+
+    socket.on("call-user", (data) => {
+
+        const socketId = userIdToSocketMapping.get(data.userID)
+        const fromUser = socketMappingToUserId.get(socketId)
+        socket.to(socketId).emit("incomming-call", { from: data.from, offer: data.offer })
+
+    })
+
+    socket.on("call-accepted", (data) => {
+
+        const { userID, ans } = data
+        const socketId = userIdToSocketMapping.get(userID)
+        socket.to(socketId).emit("call-accepted", { userID, ans })
+
+    })
+
+})
+
+app.listen(8000, () => console.log("Server listening on 8000"))
+io.listen(8001)
